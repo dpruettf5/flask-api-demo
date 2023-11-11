46caf6d310673e9ed11e1e535a88bf493298ccf2
dpruettf5 <72218850+dpruettf5@users.noreply.github.com>
2023-11-11 09:12:56 -0700

    Update __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -18,7 +18,7 @@ def init_exception(app: OpenAPI):
 
     @app.errorhandler(Exception)
     def handler(e):
-        """处理全局异常"""
+        """handle exceptions"""
         if isinstance(e, BaseAPIException):
             return e
         elif isinstance(e, HTTPException):
@@ -31,8 +31,7 @@ def init_exception(app: OpenAPI):
 
 
 def auto_register_api(app: OpenAPI):
-    """自动注册蓝图API
-    自动寻找api文件夹中的APIBlueprint并完成注册
+    """register blueprint
     """
     here = os.path.dirname(__file__)
     api_dir = os.path.join(here, "api")
@@ -50,14 +49,14 @@ def auto_register_api(app: OpenAPI):
                 api = importlib.import_module(api_route)
                 app.register_api(api.api)
             except AttributeError:
-                print(f"模块 {api_route} 中没有api变量")
+                print(f"module {api_route} auto-enrollment")
             except:
                 traceback.print_exc()
-                print(f"模块 {api_route} 自动注册错误")
+                print(f"module {api_route} auto-enrollment")
 
 
 def register_apis(app: OpenAPI):
-    """注册API蓝图"""
+    """register the api"""
     # from app.api.user import api as user_api
     # from app.api.admin import api as admin_api
     # from app.api.book import api as book_api
@@ -72,26 +71,25 @@ def register_apis(app: OpenAPI):
 
 
 def init_jwt(app: OpenAPI):
-    """初始化JWT"""
+    """init jwt"""
     from app.utils.jwt_tools import jwt_manager
     jwt_manager.init_app(app)
 
 
 def init_db(app: OpenAPI):
-    """初始化数据库"""
+    """init db"""
     from app.model import db
     db.init_app(app)
 
 
 def init_rq2(app: OpenAPI):
-    """初始化rq2"""
+    """init rq2"""
     from app.rq import rq2
     rq2.init_app(app)
 
 
 def create_app():
     from . import config
-    # 创建Flask实例
     app = OpenAPI(
         __name__,
         info=Info(title=config.APP_NAME, version=config.APP_VERSION),
@@ -102,22 +100,22 @@ def create_app():
         },
         doc_expansion="none",
     )
-    # json 正确返回中文
+
     app.json.ensure_ascii = False
-    # 使用真实IP
+
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
-    # 全局配置项
+
     app.config.from_object(config)
-    # 初始化全局异常
+
     init_exception(app)
-    # 跨域支持
+
     CORS(app)
-    # 初始化JWT
+
     init_jwt(app)
-    # 初始化数据库
+
     init_db(app)
-    # 初始化rq2
+
     init_rq2(app)
-    # 注册API蓝图
+
     register_apis(app)
     return app
